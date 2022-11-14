fecdf4c6f319c9953f95a15e3ef3aaf10c397567
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2022-11-14 08:57:17 +0100

    Apply suggestions from code review

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
 use serde::{de::DeserializeOwned, Serialize};
-#[cfg(feature = "bevy")] use bevy_ecs::prelude::Resource;
-
 
 trait StoreImpl {
     type GetError;
@@ -33,7 +31,7 @@ pub use backend::{GetError, SetError};
 ///
 /// Automatically inserted when adding `PkvPlugin`
 #[derive(Debug)]
-#[cfg_attr(feature = "bevy", derive(Resource))]
+#[cfg_attr(feature = "bevy", derive(bevy_ecs::prelude::Resource))]
 pub struct PkvStore {
     inner: backend::InnerStore,
 }

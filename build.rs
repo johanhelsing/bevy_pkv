ccec8b250cd81300184ecd2a13c8abdf521cf9ef
Dima Rets <Dmytro.Rets@Gamesys.co.uk>
2023-06-20 22:46:58 +0100

    Add redb adapter

--- build.rs
+++ build.rs
@@ -4,6 +4,7 @@ fn main() {
     cfg_aliases! {
         wasm: { target_arch = "wasm32" },
         rocksdb_backend: { all(feature = "rocksdb", not(wasm)) },
+        redb_backend: { all(feature = "redb", not(wasm)) },
         sled_backend: { all(feature = "sled", not(wasm)) }
     }
 }
